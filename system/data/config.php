fc8da3048b0f91ebbc488add48db8d1c847d5076
KudaSoff <kudasov@outlook.com>
2015-10-02 10:34:37 +0300

    Start this project

---
+++ system/data/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Определяем параметры подключения к БД указывая их в константах
+ *
+ * @const DB_HOST
+ * @const DB_NAME
+ * @const U_NAME
+ * @const U_PASS
+ */
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'time_tracking_db');
+define('U_NAME', 'root');
+define('U_PASS', '');
+
+?>
